f9d05170dd263ccbf9383904846ae2b25626a4b8
Oleg Makiienko <oleg.makiienko@@scout24.com>
2017-02-07 17:48:10 +0100

    ...

--- src/test/scala/org/functionalkoans/forscala/AboutImplicits.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutImplicits.scala
@@ -16,8 +16,8 @@ class AboutImplicits extends KoanSuite with ShouldMatchers {
 
     implicit def thisMethodNameIsIrrelevant(value: Int) = new KoanIntWrapper(value)
 
-    19.isOdd should be(__)
-    20.isOdd should be(__)
+    19.isOdd should be(true)
+    20.isOdd should be(false)
   }
 
   koan("""Implicits rules can be imported into your scope with an import""") {
@@ -34,46 +34,36 @@ class AboutImplicits extends KoanSuite with ShouldMatchers {
 
     import MyPredef._
     //imported implicits come into effect within this scope
-    19.isOdd should be(__)
-    20.isOdd should be(__)
-  }
-
-  koan("""Implicits can be used to automatically convert one type to another""") {
-
-    import java.math.BigInteger
-    implicit def Int2BigIntegerConvert(value: Int): BigInteger = new BigInteger(value.toString)
-
-    def add(a: BigInteger, b: BigInteger) = a.add(b)
-
-    add(3, 6) should be(__)
+    19.isOdd should be(true)
+    20.isOdd should be(false)
   }
 
   koan("""Implicits can be used declare a value to be provided as a default as
           |   long as an implicit value is set with in the scope.  These are
           |   called implicit function parameters""") {
 
-    def howMuchCanIMake_?(hours: Int)(implicit dollarsPerHour: BigDecimal) = dollarsPerHour * hours
+    def howMuchCanIMake_?(hours: Int)(implicit hourlyRate: BigDecimal) = hourlyRate * hours
 
     implicit var hourlyRate = BigDecimal(34.00)
 
-    howMuchCanIMake_?(30) should be(__)
+    howMuchCanIMake_?(30) should be(30 * 34.00)
 
     hourlyRate = BigDecimal(95.00)
-    howMuchCanIMake_?(95) should be(__)
+    howMuchCanIMake_?(95) should be(95 * 95.00)
   }
 
   koan("""Implicit Function Parameters can contain a list of implicits""") {
 
-    def howMuchCanIMake_?(hours: Int)(implicit amount: BigDecimal, currencyName: String) =
-      (amount * hours).toString() + " " + currencyName
+    def howMuchCanIMake_?(hours: Int)(implicit hourlyRate: BigDecimal, currencyName: String) =
+      (hourlyRate * hours).toString() + " " + currencyName
 
     implicit var hourlyRate = BigDecimal(34.00)
     implicit val currencyName = "Dollars"
 
-    howMuchCanIMake_?(30) should be(__)
+    howMuchCanIMake_?(30) should be("1020.0 Dollars")
 
     hourlyRate = BigDecimal(95.00)
-    howMuchCanIMake_?(95) should be(__)
+    howMuchCanIMake_?(95) should be("9025.0 Dollars")
   }
 
   koan("""Default arguments though are preferred to Implicit Function Parameters""") {
@@ -81,8 +71,8 @@ class AboutImplicits extends KoanSuite with ShouldMatchers {
     def howMuchCanIMake_?(hours: Int, amount: BigDecimal = 34, currencyName: String = "Dollars") =
       (amount * hours).toString() + " " + currencyName
 
-    howMuchCanIMake_?(30) should be(__)
+    howMuchCanIMake_?(30) should be("1020 Dollars")
 
-    howMuchCanIMake_?(95, 95) should be(__)
+    howMuchCanIMake_?(95, 95) should be("9025 Dollars")
   }
 }
